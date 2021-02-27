15cf041418d6920bf501378cd49f6e8c253da2da
Matheus Mósso <msmosso@gmail.com>
2021-02-26 22:07:27 -0300

    Desafio resolvido

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import Customer from '../infra/typeorm/entities/Customer';
 import ICustomersRepository from '../repositories/ICustomersRepository';
+import CustomersRepository from '../infra/typeorm/repositories/CustomersRepository';
 
 interface IRequest {
   name: string;
@@ -12,10 +13,21 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject(CustomersRepository)
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const customerExisits = await this.customersRepository.findByEmail(email);
+
+    if (customerExisits) {
+      throw new AppError('This email alrealdy assigned to a customer');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -4,8 +4,11 @@ import AppError from '@shared/errors/AppError';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import ProductsRepository from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
+import OrdersRepository from '../infra/typeorm/repositories/OrdersRepository';
 
 interface IProduct {
   id: string;
@@ -20,13 +23,58 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject(OrdersRepository)
     private ordersRepository: IOrdersRepository,
+    @inject(ProductsRepository)
     private productsRepository: IProductsRepository,
+    @inject(CustomersRepository)
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('This customer does not registered');
+    }
+
+    const validProducts = await this.productsRepository.findAllById(products);
+
+    if (validProducts.length !== products.length) {
+      throw new AppError('Invalid Order');
+    }
+
+    products.forEach(product => {
+      validProducts.forEach(p => {
+        if (p.id === product.id && product.quantity > p.quantity) {
+          throw new AppError(`Quantity of ${p.name} is not avaliable`);
+        }
+      });
+    });
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: validProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const orderProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        validProducts.filter(p => p.id === product.product_id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderProductsQuantity);
+
+    return order;
   }
 }
 
